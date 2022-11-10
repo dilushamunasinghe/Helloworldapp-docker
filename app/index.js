4b5c6a7d6e1c226870571c3b22f4e3e280d636f5
Dilusha Munasinghe <dnmu96@gmail.com>
2022-11-10 14:38:41 +0530

    Modify app folder

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(5000, () => console.log('Server is up and running'));
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => res.send('Hello World! Onexlab'))
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+    console.log(`Onexlab app listening on port ${port}`);
+});
